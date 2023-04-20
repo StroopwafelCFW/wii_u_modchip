e608f7a7b117d54dd08cb7de942deced70f7b73c
shinyquagsire23 <mtinc2@gmail.com>
2023-04-20 02:44:47 -0700

    EXI inject working

--- pico_defuse/src/pico_defuse.c
+++ pico_defuse/src/pico_defuse.c
@@ -27,10 +27,17 @@ void main()
     gpio_set_dir(PIN_LED, GPIO_OUT);
     gpio_put(PIN_LED, true);
 
+    gpio_init(PIN_NRST);
+    gpio_set_dir(PIN_NRST, GPIO_OUT);
+    gpio_put(PIN_NRST, true);
+
     // TODO: figure out the bare minimum I can pull off (maybe downclock later?)
     // 250MHz seems to capture debug pin transistions, which was the ~status quo
-    // on my FPGA at 27MHz
-    set_sys_clock_khz(250000, true);
+    // on my FPGA at 27MHz. Let's do 8x27MHz to be safe.
+    set_sys_clock_khz(216000, true);
+
+    sleep_ms(3000);
+    printf("Hello!\n");
 
     // Prioritize DMA engine
     bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
@@ -53,13 +60,12 @@ void main()
 
     // Logic
     uint debug_gpio_monitor_parallel_sm = pio_claim_unused_sm(pio, true);
+    uint exi_inject_sm = pio_claim_unused_sm(pio, true);
+
+    uint exi_inject_offset = pio_add_program(pio, &exi_inject_program);
 
     debug_gpio_monitor_parallel_program_init(pio, debug_gpio_monitor_parallel_sm, PIN_DEBUGLED_BASE, PIN_DEBUGLED_COUNT, 1.0);
-
-    //pio_sm_put(pio, debug_gpio_monitor_parallel_sm, (uint32_t) 224); // CS pulses
-    //pio_sm_exec(pio, debug_gpio_monitor_parallel_sm, pio_encode_pull(true, true));
-    //pio_sm_exec(pio, debug_gpio_monitor_parallel_sm, pio_encode_mov(pio_x, pio_osr));
-    //pio_sm_exec(pio, debug_gpio_monitor_parallel_sm, pio_encode_out(pio_null, 32));
+    pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
 
     // Set up DMA for reading results
     int chan = dma_claim_unused_channel(true);
@@ -69,99 +75,96 @@ void main()
     channel_config_set_read_increment(&c, false);
     channel_config_set_write_increment(&c, true);
     channel_config_set_dreq(&c, pio_get_dreq(pio, debug_gpio_monitor_parallel_sm, false));
-
-    dma_channel_configure(
-        chan,
-        &c,
-        read_debug_gpios,
-        &pio->rxf[debug_gpio_monitor_parallel_sm],
-        0x20, //  sizeof(read_debug_gpios)
-        true // start immediately
-    );
-
-    sleep_ms(3000);
-
-    pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
     
-    int round = 0;
-    while (1)
+    int winner = 0;
+    for (int reset_attempt = 0; reset_attempt < 0x10000; reset_attempt++)
     {
-        printf("Waiting... %x\n", round++);
-        dma_channel_wait_for_finish_blocking(chan);
+        printf("init exi inject\n");
+        pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
+        exi_inject_program_init(pio, exi_inject_sm, exi_inject_offset, PIN_CLK, PIN_DATA_BASE, 1.0);
 
+        printf("warmup run\n");
+
+        // Read OTP fully at least once and then hold
+        gpio_put(PIN_NRST, false);
+        gpio_put(PIN_NRST, true);
+        sleep_ms(30);
+        gpio_put(PIN_NRST, false);
+
+        // Clear out the FIFO
         for (int i = 0; i < 0x20; i++)
         {
-            //read_debug_gpios[i] = pio_sm_get_blocking(pio, debug_gpio_monitor_parallel_sm);
-            printf("%02x\n", read_debug_gpios[i]);
+            pio_sm_get(pio, debug_gpio_monitor_parallel_sm);
         }
+
+        printf("Starting... %u\n", reset_attempt);
+
+        // The actual timing-sensitive part...
+        gpio_put(PIN_NRST, true);
+        for(int i = 0; i < 0x10000 - reset_attempt; i++)
+        {
+            __asm volatile ("nop\n");
+        }
+        gpio_put(PIN_NRST, false);
+        printf("Check...\n");
+
+        // Clear out the FIFO
+        for (int i = 0; i < 0x20; i++)
+        {
+            pio_sm_get(pio, debug_gpio_monitor_parallel_sm);
+        }
+
         memset(read_debug_gpios, 0, sizeof(read_debug_gpios));
-        //sleep_ms(1000);
-        //dma_channel_start(chan);
         dma_channel_configure(
             chan,
             &c,
             read_debug_gpios,
             &pio->rxf[debug_gpio_monitor_parallel_sm],
-            0x20, //  sizeof(read_debug_gpios)
+            sizeof(read_debug_gpios),
             true // start immediately
         );
+
+        pio_sm_set_enabled(pio, exi_inject_sm, true);
+        gpio_put(PIN_NRST, true);
+        sleep_ms(60);
+        dma_channel_abort(chan);
+        pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, false);
+        pio_sm_set_enabled(pio, exi_inject_sm, false);
+
+        // Winner?
+        printf("Results:\n");
+        for (int i = 0; i < 0x100; i++)
+        {
+            //read_debug_gpios[i] = pio_sm_get_blocking(pio, debug_gpio_monitor_parallel_sm);
+            printf("%02x\n", read_debug_gpios[i]);
+            if (i && read_debug_gpios[i] == read_debug_gpios[i-1]) {
+                break;
+            }
+            if (read_debug_gpios[i] == 0x88 || read_debug_gpios[i] == 0x25) {
+                winner = 1;
+            }
+            if (read_debug_gpios[i] == 0xE1) {
+                winner = 0;
+            }
+        }
+
+        if (winner) {
+            printf("Winner! 0x%x\n", 0x10000 - reset_attempt);
+            break;
+        }
+
+        //sleep_ms(1000);
     }
 
-#if 0
-    //
-    // State Machine: Transfer Start
-    //
-    // Counts all consecutive transfers and sets IRQ 
-    // when first 1 kilobyte transfer starts.
-    //
-
-    uint transfer_start_sm = pio_claim_unused_sm(pio, true);
-    uint transfer_start_offset = pio_add_program(pio, &on_transfer_program);
-
-    on_transfer_program_init(pio, transfer_start_sm, transfer_start_offset, PIN_CLK, PIN_CS, PIN_DATA_BASE);
-
-    pio_sm_put(pio, transfer_start_sm, (uint32_t) 224); // CS pulses
-    pio_sm_exec(pio, transfer_start_sm, pio_encode_pull(true, true));
-    pio_sm_exec(pio, transfer_start_sm, pio_encode_mov(pio_x, pio_osr));
-    pio_sm_exec(pio, transfer_start_sm, pio_encode_out(pio_null, 32));
-
-    //
-    // State Machine: Clocked Output
-    // 
-    // It waits for IRQ signal from first SM and samples clock signal
-    // to output IPL data bits.
-    //
-
-    uint clocked_output_sm = pio_claim_unused_sm(pio, true);
-    uint clocked_output_offset = pio_add_program(pio, &clocked_output_program);
-
-    clocked_output_program_init(pio, clocked_output_sm, clocked_output_offset, PIN_DATA_BASE, PIN_CLK, PIN_CS);
-
-    pio_sm_put(pio, clocked_output_sm, 8191); // 8192 bits, 1024 bytes, minus 1 because counting starts from 0 
-    pio_sm_exec(pio, clocked_output_sm, pio_encode_pull(true, true));
-    pio_sm_exec(pio, clocked_output_sm, pio_encode_mov(pio_y, pio_osr));
-    pio_sm_exec(pio, clocked_output_sm, pio_encode_out(pio_null, 32));
-
-    // Start PIO state machines
-    pio_sm_set_enabled(pio, transfer_start_sm, true);
-    pio_sm_set_enabled(pio, clocked_output_sm, true);
-#endif
-
-    gpio_init(PIN_NRST);
-    gpio_set_dir(PIN_NRST, GPIO_OUT);
-    gpio_put(PIN_NRST, true);
-
-    /*
-    gpio_put(PIN_NRST, true);
-        gpio_put(PIN_LED, true);
-        sleep_ms(1000);
-        gpio_put(PIN_NRST, false);
-        gpio_put(PIN_LED, false);
-        sleep_ms(1000);
-    */
+    // simple monitor
+    pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
+    while (1)
+    {
+        printf("%02x\n", pio_sm_get_blocking(pio, debug_gpio_monitor_parallel_sm));
+    }
 
     while (true) {
-        printf("Hello, world!\n");
+        //printf("Hello, world!\n");
         tight_loop_contents();
     }
 }
