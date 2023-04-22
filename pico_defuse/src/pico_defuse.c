16177c1ebc6b975d615eeb3ae90d5a9bc1e2eb80
shinyquagsire23 <mtinc2@gmail.com>
2023-04-21 17:39:46 -0700

    Serial console input, normal boot fallback

--- pico_defuse/src/pico_defuse.c
+++ pico_defuse/src/pico_defuse.c
@@ -8,8 +8,8 @@
 #include "hardware/sync.h" 
 #include "pico_defuse.pio.h"
 
-const uint PIN_DEBUGLED_BASE = 2;
-const uint PIN_DEBUGLED_COUNT = 8;
+const uint PIN_DEBUGLED_BASE = 2;   // 8 consecutive pins
+const uint PIN_SERIALOUT_BASE = 18; // 4 consecutive pins tied together
 
 const uint PIN_LED = 25;                // Status LED
 const uint PIN_DATA_BASE = 10;          // Base pin used for output, 4 consecutive pins are used 
@@ -36,27 +36,103 @@ uint debug_gpio_monitor_serial_offset;
 #define WIIU_STATE_DEFUSED        (2)
 #define WIIU_STATE_MONITORING     (3)
 #define WIIU_CHECK_IF_POWERED_OFF (4)
+#define WIIU_STATE_NORMAL_BOOT    (5)
 
-const char* WIIU_STATE_NAMES[5] = {
+const char* WIIU_STATE_NAMES[6] = {
     "WIIU_STATE_POWERED_OFF",
     "WIIU_STATE_NEEDS_DEFUSE",
     "WIIU_STATE_DEFUSED",
     "WIIU_STATE_MONITORING",
     "WIIU_CHECK_IF_POWERED_OFF",
+    "WIIU_STATE_NORMAL_BOOT"
+};
+
+const uint32_t serial_LUT[256] = {
+    0x00000000,0x0000000f,0x000000f0,0x000000ff,
+    0x00000f00,0x00000f0f,0x00000ff0,0x00000fff,
+    0x0000f000,0x0000f00f,0x0000f0f0,0x0000f0ff,
+    0x0000ff00,0x0000ff0f,0x0000fff0,0x0000ffff,
+    0x000f0000,0x000f000f,0x000f00f0,0x000f00ff,
+    0x000f0f00,0x000f0f0f,0x000f0ff0,0x000f0fff,
+    0x000ff000,0x000ff00f,0x000ff0f0,0x000ff0ff,
+    0x000fff00,0x000fff0f,0x000ffff0,0x000fffff,
+    0x00f00000,0x00f0000f,0x00f000f0,0x00f000ff,
+    0x00f00f00,0x00f00f0f,0x00f00ff0,0x00f00fff,
+    0x00f0f000,0x00f0f00f,0x00f0f0f0,0x00f0f0ff,
+    0x00f0ff00,0x00f0ff0f,0x00f0fff0,0x00f0ffff,
+    0x00ff0000,0x00ff000f,0x00ff00f0,0x00ff00ff,
+    0x00ff0f00,0x00ff0f0f,0x00ff0ff0,0x00ff0fff,
+    0x00fff000,0x00fff00f,0x00fff0f0,0x00fff0ff,
+    0x00ffff00,0x00ffff0f,0x00fffff0,0x00ffffff,
+    0x0f000000,0x0f00000f,0x0f0000f0,0x0f0000ff,
+    0x0f000f00,0x0f000f0f,0x0f000ff0,0x0f000fff,
+    0x0f00f000,0x0f00f00f,0x0f00f0f0,0x0f00f0ff,
+    0x0f00ff00,0x0f00ff0f,0x0f00fff0,0x0f00ffff,
+    0x0f0f0000,0x0f0f000f,0x0f0f00f0,0x0f0f00ff,
+    0x0f0f0f00,0x0f0f0f0f,0x0f0f0ff0,0x0f0f0fff,
+    0x0f0ff000,0x0f0ff00f,0x0f0ff0f0,0x0f0ff0ff,
+    0x0f0fff00,0x0f0fff0f,0x0f0ffff0,0x0f0fffff,
+    0x0ff00000,0x0ff0000f,0x0ff000f0,0x0ff000ff,
+    0x0ff00f00,0x0ff00f0f,0x0ff00ff0,0x0ff00fff,
+    0x0ff0f000,0x0ff0f00f,0x0ff0f0f0,0x0ff0f0ff,
+    0x0ff0ff00,0x0ff0ff0f,0x0ff0fff0,0x0ff0ffff,
+    0x0fff0000,0x0fff000f,0x0fff00f0,0x0fff00ff,
+    0x0fff0f00,0x0fff0f0f,0x0fff0ff0,0x0fff0fff,
+    0x0ffff000,0x0ffff00f,0x0ffff0f0,0x0ffff0ff,
+    0x0fffff00,0x0fffff0f,0x0ffffff0,0x0fffffff,
+    0xf0000000,0xf000000f,0xf00000f0,0xf00000ff,
+    0xf0000f00,0xf0000f0f,0xf0000ff0,0xf0000fff,
+    0xf000f000,0xf000f00f,0xf000f0f0,0xf000f0ff,
+    0xf000ff00,0xf000ff0f,0xf000fff0,0xf000ffff,
+    0xf00f0000,0xf00f000f,0xf00f00f0,0xf00f00ff,
+    0xf00f0f00,0xf00f0f0f,0xf00f0ff0,0xf00f0fff,
+    0xf00ff000,0xf00ff00f,0xf00ff0f0,0xf00ff0ff,
+    0xf00fff00,0xf00fff0f,0xf00ffff0,0xf00fffff,
+    0xf0f00000,0xf0f0000f,0xf0f000f0,0xf0f000ff,
+    0xf0f00f00,0xf0f00f0f,0xf0f00ff0,0xf0f00fff,
+    0xf0f0f000,0xf0f0f00f,0xf0f0f0f0,0xf0f0f0ff,
+    0xf0f0ff00,0xf0f0ff0f,0xf0f0fff0,0xf0f0ffff,
+    0xf0ff0000,0xf0ff000f,0xf0ff00f0,0xf0ff00ff,
+    0xf0ff0f00,0xf0ff0f0f,0xf0ff0ff0,0xf0ff0fff,
+    0xf0fff000,0xf0fff00f,0xf0fff0f0,0xf0fff0ff,
+    0xf0ffff00,0xf0ffff0f,0xf0fffff0,0xf0ffffff,
+    0xff000000,0xff00000f,0xff0000f0,0xff0000ff,
+    0xff000f00,0xff000f0f,0xff000ff0,0xff000fff,
+    0xff00f000,0xff00f00f,0xff00f0f0,0xff00f0ff,
+    0xff00ff00,0xff00ff0f,0xff00fff0,0xff00ffff,
+    0xff0f0000,0xff0f000f,0xff0f00f0,0xff0f00ff,
+    0xff0f0f00,0xff0f0f0f,0xff0f0ff0,0xff0f0fff,
+    0xff0ff000,0xff0ff00f,0xff0ff0f0,0xff0ff0ff,
+    0xff0fff00,0xff0fff0f,0xff0ffff0,0xff0fffff,
+    0xfff00000,0xfff0000f,0xfff000f0,0xfff000ff,
+    0xfff00f00,0xfff00f0f,0xfff00ff0,0xfff00fff,
+    0xfff0f000,0xfff0f00f,0xfff0f0f0,0xfff0f0ff,
+    0xfff0ff00,0xfff0ff0f,0xfff0fff0,0xfff0ffff,
+    0xffff0000,0xffff000f,0xffff00f0,0xffff00ff,
+    0xffff0f00,0xffff0f0f,0xffff0ff0,0xffff0fff,
+    0xfffff000,0xfffff00f,0xfffff0f0,0xfffff0ff,
+    0xffffff00,0xffffff0f,0xfffffff0,0xffffffff
 };
 
 const uint8_t MONITOR_SERIAL_DATA_MAGIC[12] = {0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0xBE, 0xEF, 0xCA, 0xFE};
 const uint8_t MONITOR_SERIAL_TEXT_MAGIC[12] = {0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0xF0, 0x0F, 0xCA, 0xFE};
+const uint8_t MONITOR_PARALLEL_DATA_MAGIC[12] = {0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0xBE, 0xEF, 0xBE, 0xEF};
+const uint8_t MONITOR_RESET_MAGIC[12] = {0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x4E, 0x52, 0x53, 0x54};
+
 
 char text_buffer[2048];
 int text_buffer_pos = 0;
 uint8_t last_16_bytes[16] = {0};
 uint8_t serial_data_cnt = 0;
 uint8_t current_mode = MONITOR_SERIAL_DATA;
+uint8_t num_serial_loops = 0;
 
 uint8_t wiiu_state = WIIU_STATE_POWERED_OFF;
 uint8_t next_wiiu_state = WIIU_STATE_POWERED_OFF;
 bool nrst_sense_state = false;
+
+#define RESET_RANGE_MIN (0x440)
+#define RESET_RANGE_MAX (0x480)
 
 void nrst_sense_callback(uint gpio, uint32_t events) 
 {
@@ -156,7 +232,7 @@ void slow_one_time_init()
     debug_gpio_monitor_serial_offset = pio_add_program(pio, &debug_gpio_monitor_serial_program);
 
     // Debug monitor can start early, it's basically always on.
-    debug_gpio_monitor_parallel_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_parallel_offset, PIN_DEBUGLED_BASE, PIN_DEBUGLED_COUNT, 1.0);
+    debug_gpio_monitor_parallel_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_parallel_offset, PIN_DEBUGLED_BASE, PIN_SERIALOUT_BASE, 1.0);
     pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
 
     // Set up DMA for reading results, but don't start it yet.
@@ -174,15 +250,17 @@ void de_fuse()
     nrst_sense_set(false);
 
     // Make sure parallel debug monitor is on
-    debug_gpio_monitor_parallel_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_parallel_offset, PIN_DEBUGLED_BASE, PIN_DEBUGLED_COUNT, 1.0);
+    debug_gpio_monitor_parallel_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_parallel_offset, PIN_DEBUGLED_BASE, PIN_SERIALOUT_BASE, 1.0);
     pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
 
     // winning range (at 8x27MHz): 0xB40 ~ 0x703 -- long tail? 0xbb9 ~ 0x461 (optimal: 0x900, 0x48 bytes OTP loaded)
     // winning range (at 4x27MHz): 0x5a0 ~ 0x381 -- (optimal: 0x480, 0x48 bytes OTP loaded)
     int winner = 0;
-    for (int reset_attempt = (0x10000-0x480); reset_attempt < 0x10000; reset_attempt++)
+    uint8_t error_code = 0;
+    for (int reset_attempt = RESET_RANGE_MAX; reset_attempt >= RESET_RANGE_MIN; reset_attempt--)
     {
         winner = 0;
+        error_code = 0;
         printf("Starting... %u\n", reset_attempt);
         pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
         exi_inject_program_init(pio_exi, exi_inject_sm, exi_inject_offset, PIN_CLK, PIN_DATA_BASE, 1.0);
@@ -214,7 +292,7 @@ void de_fuse()
         uint32_t cookie = save_and_disable_interrupts();
         gpio_put(PIN_NRST, true);
 
-        for(int i = 0; i < 0x10000 - reset_attempt; i++)
+        for(int i = 0; i < reset_attempt; i++)
         {
             __asm volatile ("\n");
         }
@@ -256,7 +334,10 @@ void de_fuse()
         // Give it enough time to reach boot1, then
         // disable all the debug monitor and EXI injector so we
         // can check the results
-        sleep_ms(100);
+        if (reset_attempt <= RESET_RANGE_MIN+4) {
+            sleep_ms(500);
+        }
+        sleep_ms(200);
         dma_channel_abort(debug_gpio_monitor_dmachan);
         pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, false);
         pio_sm_set_enabled(pio_exi, exi_inject_sm, false);
@@ -274,6 +355,10 @@ void de_fuse()
             if (read_debug_gpios[i] == 0xE1) {
                 winner = 0;
             }
+
+            if (read_debug_gpios[i] >= 0xC0) {
+                error_code = read_debug_gpios[i];
+            }
         }
 
         if (winner) {
@@ -289,15 +374,42 @@ void de_fuse()
             next_wiiu_state = WIIU_STATE_DEFUSED;
             break;
         }
+        else {
+            for (int i = 0; i < 0x100; i++)
+            {
+                printf("%02x\n", read_debug_gpios[i]);
+                if (i && read_debug_gpios[i] == read_debug_gpios[i-1]) {
+                    break;
+                }
+            }
+            printf("Error code: %02x\n", error_code);
+        }
 
         //sleep_ms(1000);
+    }
+
+    // TODO: do this after trying a NAND boot1?
+    // If no SD card is inserted, or an invalid one is inserted, boot0 stalls.
+    if (!winner && error_code == 0x00) {
+        printf("SD card not valid or not inserted, doing a normal boot.\n");
+        gpio_put(PIN_NRST, false);
+        for(int i = 0; i < 0x100; i++)
+        {
+            __asm volatile ("\n");
+        }
+        gpio_put(PIN_NRST, true);
+        for(int i = 0; i < 0x100; i++)
+        {
+            __asm volatile ("\n");
+        }
+        next_wiiu_state = WIIU_STATE_NORMAL_BOOT;
     }
 }
 
 void post_de_fuse()
 {
     // Switch to serial monitor and monitor NRST
-    debug_gpio_monitor_serial_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_serial_offset, PIN_DEBUGLED_BASE, PIN_DEBUGLED_COUNT, 1.0);
+    debug_gpio_monitor_serial_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_serial_offset, PIN_DEBUGLED_BASE, PIN_SERIALOUT_BASE, 1.0);
     nrst_sense_set(true);
     memset(text_buffer, 0, sizeof(text_buffer));
     text_buffer_pos = 0;
@@ -305,13 +417,22 @@ void post_de_fuse()
     pio_sm_set_enabled(pio, debug_gpio_monitor_parallel_sm, true);
     memset(last_16_bytes, 0, sizeof(last_16_bytes));
     serial_data_cnt = 0;
-    current_mode = MONITOR_SERIAL_DATA;
+    current_mode = MONITOR_SERIAL_TEXT;
 
     next_wiiu_state = WIIU_STATE_MONITORING;
 }
 
 void wiiu_serial_monitor()
 {
+    if (!pio_sm_is_tx_fifo_full(pio, debug_gpio_monitor_parallel_sm))
+    {
+        int char_in = getchar_timeout_us(0);
+        if (char_in != PICO_ERROR_TIMEOUT && (char_in >= 0 && char_in <= 0xFF)) {
+            //printf("[pico] char! %x\n", char_in);
+            pio_sm_put(pio, debug_gpio_monitor_parallel_sm, serial_LUT[char_in]);
+        }
+    }
+    
     if (pio_sm_is_rx_fifo_empty(pio, debug_gpio_monitor_parallel_sm)) {
         return;
     }
@@ -323,7 +444,19 @@ void wiiu_serial_monitor()
     }
     last_16_bytes[15] = read_val;
 
-    if (!memcmp(last_16_bytes+4, MONITOR_SERIAL_DATA_MAGIC, 12)) {
+    if (!memcmp(last_16_bytes+4, MONITOR_RESET_MAGIC, 12))
+    {
+        next_wiiu_state = WIIU_STATE_NEEDS_DEFUSE;
+        printf("[Pico] Console requested reset...\n");
+        sleep_ms(500); // give it a delay
+    }
+    else if (!memcmp(last_16_bytes+4, MONITOR_PARALLEL_DATA_MAGIC, 12)) {
+        debug_gpio_monitor_parallel_program_init(pio, debug_gpio_monitor_parallel_sm, debug_gpio_monitor_parallel_offset, PIN_DEBUGLED_BASE, PIN_SERIALOUT_BASE, 1.0);
+        current_mode = MONITOR_SERIAL_DATA;
+        serial_data_cnt = 0;
+        printf("[Pico] Switching to parallel data mode...\n");
+    }
+    else if (!memcmp(last_16_bytes+4, MONITOR_SERIAL_DATA_MAGIC, 12)) {
         current_mode = MONITOR_SERIAL_DATA;
         serial_data_cnt = 0;
         printf("[Pico] Switching to data mode...\n");
@@ -336,6 +469,8 @@ void wiiu_serial_monitor()
         text_buffer_pos = 0;
     }
 
+    num_serial_loops++;
+
     if (current_mode == MONITOR_SERIAL_DATA)
     {
         if (serial_data_cnt++ % 16 == 0) {
@@ -345,12 +480,17 @@ void wiiu_serial_monitor()
     }
     else if (current_mode == MONITOR_SERIAL_TEXT)
     {
-        if (text_buffer_pos >= sizeof(text_buffer) - 4) {
+        if (text_buffer_pos >= sizeof(text_buffer) - 4 || num_serial_loops > 16) {
             printf("%s", text_buffer);
             memset(text_buffer, 0, text_buffer_pos);
             text_buffer_pos = 0;
+            num_serial_loops = 0;
         }
-        if (read_val == '\t' || read_val == '\n' || read_val == '\r' || (read_val >= ' ' && read_val <= '~')) {
+        if (!read_val)
+        {
+            // reading input
+        }
+        else if (read_val == 0x1B || read_val == '\t' || read_val == '\n' || read_val == '\r' || (read_val >= ' ' && read_val <= '~')) {
             if ((read_val == '\r' && last_16_bytes[14] != '\n') || read_val == '\n') {
                 printf("%s\n", text_buffer);
                 memset(text_buffer, 0, text_buffer_pos);
@@ -362,6 +502,7 @@ void wiiu_serial_monitor()
         }
         else {
             sprintf(text_buffer + text_buffer_pos, "%02x", read_val);
+            text_buffer_pos += 2;
         }
     }
     else {
@@ -383,8 +524,8 @@ void main()
     printf("Start state machine.\n");
     if (gpio_get(PIN_NRST)) {
         printf("[pico] Uhhh the console is on? Monitoring...\n");
-        wiiu_state = WIIU_STATE_MONITORING;
-        next_wiiu_state = WIIU_STATE_MONITORING;
+        wiiu_state = WIIU_STATE_DEFUSED;
+        next_wiiu_state = WIIU_STATE_DEFUSED;
     }
 
     // State machines yayyyy
@@ -392,6 +533,7 @@ void main()
     {
         switch (wiiu_state)
         {
+        case WIIU_STATE_NORMAL_BOOT:
         case WIIU_STATE_POWERED_OFF:
             nrst_sense_set(true); // wait for IRQs
             break;
